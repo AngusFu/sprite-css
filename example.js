483e924d376ba97d3da26f71099bfda43b32e002
wemlion <angusfu1126@qq.com>
2018-11-19 23:41:44 +0800

    update

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ const cssPath = resolve(__dirname, './fixtures/test.css')
 const run = async () => {
   try {
     const result = await transform({ path: cssPath })
-    console.log(result)
+    console.log(JSON.stringify(result))
   } catch (e) {
     console.error(e)
   }
